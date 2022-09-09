d33c92a1972a91c5a226234a4615c57c3c655e8f
Haneen Mahdin <haneenmahdin@gmail.com>
2022-09-09 21:30:36 +0530

    (deps) add json parser & ansi text styles

---
+++ include/ansi-text/ansi-text.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#pragma clang diagnostic ignored "-Wpedantic"
+
+// Colors
+#define BLACK "\e[0;30m"
+#define RED "\e[0;31m"
+#define GREEN "\e[0;32m"
+#define YELLOW "\e[0;33m"
+#define BLUE "\e[0;34m"
+
+// Background
+#define BLACK_BG "\e[40m"
+#define RED_BG "\e[41m"
+#define GREEN_BG "\e[42m"
+
+// Styles
+#define BOLD "\e[1m"
+#define NORMAL "\e[m"
+
+// Reset
+#define COLOR_RESET "\e[0m"
