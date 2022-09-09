6fa22784bc23e1812a4ad6c40461a5a2e1865a97
Haneen Mahdin <haneenmahdin@gmail.com>
2022-09-09 21:27:52 +0530

    (core) add helper class to read config from the filesystem

--- src/Config.hpp
+++ src/Config.hpp
@@ -6,9 +6,38 @@
 #include <string>
 #include <filesystem>
 #include <functional>
+#include <fstream>
+#include <nlohmann/json.hpp>
 
-struct Config { 
-    std::optional<std::string> projectName;
+using json = nlohmann::json;
+
+struct Config
+{
+    std::string projectName;
     // a target could be either a file or a directory
-    std::optional<std::string> target;
+    std::string target;
+    // Commands
+    std::string createdCommand;
+    std::string modifiedCommand;
+    std::string erasedCommand;
+};
+
+class ConfigReader
+{
+public:
+    static Config read(std::string config_path)
+    {
+        struct Config config;
+        std::ifstream rawConfig(config_path);
+
+        json configContent = json::parse(rawConfig);
+
+        config.projectName = configContent["projectName"];
+        config.target = configContent["target"];
+        config.createdCommand = configContent["createdCommand"];
+        config.modifiedCommand = configContent["modifiedCommand"];
+        config.erasedCommand = configContent["erasedCommand"];
+
+        return config;
+    }
 };
